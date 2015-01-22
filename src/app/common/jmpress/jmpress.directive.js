0acf8f822f5e616a723023cc983c07fb932c7661
Miki <warlord_bv@yahoo.com>
2015-01-22 12:55:34 +0100

    made the jmpress directive work

--- src/app/common/jmpress/jmpress.directive.js
+++ src/app/common/jmpress/jmpress.directive.js
@@ -3,26 +3,16 @@
 
     define(['jmpress'], function (jmpress) {
 
+        var deinitDirective = function (element) {
+            element.jmpress("deinit");
+        };
 
         var initDirective = function (element) {
             $.jmpress("initStep", function (step, eventData) {
                 eventData.stepData.up = eventData.data.up;
                 eventData.stepData.down = eventData.data.down;
             });
-            try {
-                $.jmpress("register", "up", function () {
-                    var stepData = element.jmpress("active").data("stepData");
-                    if (stepData.up)
-                        element.jmpress("select", stepData.up);
-                });
-                $.jmpress("register", "down", function () {
-                    var stepData = element.jmpress("active").data("stepData");
-                    if (stepData.down)
-                        element.jmpress("select", stepData.down);
-                });
-            }catch(e){
-                //prevented double init
-            }
+
             element.jmpress({
                 stepSelector: 'section',
                 hash: false,
@@ -42,6 +32,17 @@
             element.jmpress("route", ["#top", "#left"], true, true);
             element.jmpress("route", ["#bottom", "#left"], true, true);
             element.jmpress("route", ["#bottom", "#right"], true);
+
+            element.jmpress("register", "up", function () {
+                var stepData = element.jmpress("active").data("stepData");
+                if (stepData.up)
+                    element.jmpress("select", stepData.up);
+            });
+            element.jmpress("register", "down", function () {
+                var stepData = element.jmpress("active").data("stepData");
+                if (stepData.down)
+                    element.jmpress("select", stepData.down);
+            });
         };
 
 
@@ -57,7 +58,9 @@
                     $timeout(function () {
                         initDirective($($element));
                     }, 100);
-
+                    $scope.$on('$destroy', function () {
+                        deinitDirective($($element));
+                    });
                 }
             };
         };
